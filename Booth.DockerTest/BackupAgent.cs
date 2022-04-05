180c83f2185f50b089f40972fa115b53525a4c73
Craig Booth <craigb@neller.com.au>
2022-04-05 19:17:19 +0930

    26

--- Booth.DockerTest/BackupAgent.cs
+++ Booth.DockerTest/BackupAgent.cs
@@ -82,19 +82,26 @@ namespace Booth.DockerTest
                 _Logger?.LogInformation("service tasktemplate: " + service.Spec.TaskTemplate);
                 _Logger?.LogInformation("service containerspec: " + service.Spec.TaskTemplate.ContainerSpec);
                 _Logger?.LogInformation("service mounts: " + service.Spec.TaskTemplate.ContainerSpec.Mounts);
-                foreach (var mount in service.Spec.TaskTemplate.ContainerSpec.Mounts)
-                {
-                    _Logger?.LogInformation("volume: " + mount.Source);
 
-                    if (backupDefinition.Volumes.Contains(mount.Source))
+                if (service.Spec.TaskTemplate.ContainerSpec.Mounts != null)
+                {
+                    _Logger?.LogInformation("checking mounts");
+
+                    foreach (var mount in service.Spec.TaskTemplate.ContainerSpec.Mounts)
                     {
-                        _Logger?.LogInformation("add version: " + service.Version.Index);
-                        _Logger?.LogInformation("add spec: " + service.Spec);
-                        affectedServices.Add(new ServiceDefinition(service.ID, (int)service.Version.Index, service.Spec));
-                        _Logger?.LogInformation("Added");
-                        break;
+                        _Logger?.LogInformation("volume: " + mount.Source);
+
+                        if (backupDefinition.Volumes.Contains(mount.Source))
+                        {
+                            _Logger?.LogInformation("add version: " + service.Version.Index);
+                            _Logger?.LogInformation("add spec: " + service.Spec);
+                            affectedServices.Add(new ServiceDefinition(service.ID, (int)service.Version.Index, service.Spec));
+                            _Logger?.LogInformation("Added");
+                            break;
+                        }
                     }
-                } 
+                }
+                
 
                 _Logger?.LogInformation("done" );
             }
